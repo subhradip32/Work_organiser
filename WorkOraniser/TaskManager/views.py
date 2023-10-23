e19b6f131438f2708fd878485e6ea832e7f3a469
Subhradip32 <subhradipmajumder309@gmail.com>
2023-10-23 18:01:57 +0530

    Added delete,complete,postpond of task features

--- WorkOraniser/TaskManager/views.py
+++ WorkOraniser/TaskManager/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import * 
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your views here.
 def load_landingPage(request):
@@ -8,16 +10,40 @@ def load_landingPage(request):
         task_des = request.POST.get('des')
         task_time = request.POST.get('datetime')
         task_tag = request.POST.get('tag_sel')
-        print(task_title,task_des,task_time,task_tag)
+        # if task_time < timezone.now():
+        #     task_status = 'lt'
         task_instance = Tasks(Title = task_title,Description = task_des,Timeline = task_time,Tag = Tag.objects.get(Tg_name = task_tag))
         task_instance.save()
+        return redirect('main_page')
 
-    # elif request.method == "GET":
-    #     data = Tasks.objects.all()
-    #     for i in data: 
-    #         print(i.Title)
+    elif request.method == "GET":
+        data = Tasks.objects.all()
+        for i in data: 
+            if i.Timeline < timezone.now():
+                i.Status = 'lt'
+                i.save()
         
     return render(request,'landing.html',{
         'alltags': Tag.objects.all(),
         'alltasks': Tasks.objects.all()
     })
+
+
+def delete_task(request,task_id):
+    inst = Tasks.objects.get(id = task_id)
+    inst.Status = 'dt'
+    inst.save()
+    return redirect('main_page')
+
+def postpond_task(request,task_id):
+    inst = Tasks.objects.get(id = task_id)
+    inst.Timeline = inst.Timeline + timedelta(days=1)
+    inst.Status = 'ot' 
+    inst.save()
+    return redirect('main_page')
+
+def complete_task(request,task_id):
+    inst = Tasks.objects.get(id = task_id)
+    inst.Is_completed = True 
+    inst.save()
+    return redirect('main_page')
