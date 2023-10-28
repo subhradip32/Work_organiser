6e7e4b3f613cc48ed6bab8986021d8327ca22acd
Subhradip32 <subhradipmajumder309@gmail.com>
2023-10-28 19:49:54 +0530

    added mysql

--- WorkOraniser/TaskManager/url.py
+++ WorkOraniser/TaskManager/url.py
@@ -1,7 +1,7 @@
 from django.urls import include,path
 from . import views 
 
-urlpatterns = [ 
+urlpatterns = [
     path('',views.load_landingPage,name="main_page"),
     path('delete/<task_id>',views.delete_task,name="delte_task"),
     path('post/<task_id>',views.postpond_task,name="postpond_task"),
--- WorkOraniser/WorkOraniser/settings.py
+++ WorkOraniser/WorkOraniser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a$e%83ti1j(jkt9(v2nq87o+_#4=zv@u!4yn((t$u)bg)gzv5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'WorkOraniser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'workorg',
+        'USER': 'root',
+        'PASSWORD': '2005',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
